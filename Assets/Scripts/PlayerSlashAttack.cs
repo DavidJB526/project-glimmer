93475bee7ba380b9cd7ee4a19ba334d8e74dedaf
DavidJB526 <david.beaudoin@loop.colum.edu>
2019-03-24 08:51:29 -0500

    Add Basic Slash Functionality to Test Scene

---
+++ Assets/Scripts/PlayerSlashAttack.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSlashAttack : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody slashObject;
+    [SerializeField]
+    private Transform slashSpawner;
+    [SerializeField]
+    private float slashSpeed;
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Slash Attack"))
+        {
+            anim.SetTrigger("SlashAttack");
+        }
+    }
+
+    private void SpawnSlashRight()
+    {
+        Rigidbody slashClone;
+
+        slashClone = Instantiate(slashObject, slashSpawner.position, slashSpawner.rotation * Quaternion.Euler(0, 0, 45)) as Rigidbody;
+        slashClone.velocity = transform.TransformDirection(slashSpawner.forward * slashSpeed);
+        anim.ResetTrigger("SlashAttack");
+    }
+
+    private void SpawnSlashLeft()
+    {
+        Rigidbody slashClone;
+
+        slashClone = Instantiate(slashObject, slashSpawner.position, slashSpawner.rotation * Quaternion.Euler(0, 0, -45)) as Rigidbody;
+        slashClone.velocity = transform.TransformDirection(slashSpawner.forward * slashSpeed);
+        anim.ResetTrigger("SlashAttack");
+    }
+}
---
+++ Assets/Scripts/SlashScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlashScript : MonoBehaviour
+{
+    [SerializeField]
+    private float lifetime = 3f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            //Deal Damage here
+        }
+
+        Destroy(gameObject);
+    }
+}
