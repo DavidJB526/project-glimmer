031bf52d53606745c00706f284388528222d5ea0
DavidJB526 <david.beaudoin@loop.colum.edu>
2019-03-30 20:41:56 -0500

    Add Sphere to Health VFX with UpDownBob

---
+++ Assets/Scripts/Utility/UpDownBob.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpDownBob : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float height;
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time * speed, height), transform.position.z);
+    }
+}
