96d22c1b6065734939350b40bdfe23a04fa278e1
DavidJB526 <david.beaudoin@loop.colum.edu>
2019-04-04 14:31:00 -0500

    Move Scripts into Folders, Add RotateObject Script, Start PlayerAttack Script

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField]
+    private float attackDistance = 5;
+    [SerializeField]
+    private float raycastRadius = 2;
+    [SerializeField]
+    private int playerLayer = 9;
+
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Debug.DrawRay(transform.position, transform.forward * attackDistance, Color.magenta);
+
+        if (Input.GetButtonDown("Light Attack"))
+        {
+            anim.SetTrigger("LightAttack");
+        }
+        else if (Input.GetButtonDown("Heavy Attack"))
+        {
+            anim.SetTrigger("HeavyAttack");
+        }
+    }
+
+    private void DamageEnemy()
+    {
+        RaycastHit hitInfo;
+
+        Physics.SphereCast(transform.position, raycastRadius, transform.forward, out hitInfo, attackDistance, playerLayer);
+    }
+}
---
+++ Assets/Scripts/Utility/RotateObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 rotationVector;
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(rotationVector);
+    }
+}
