6c8e31b713edbc0d9ea474d7ec2e7f01d2f71cac
DavidJB526 <david.beaudoin@loop.colum.edu>
2019-04-09 09:59:38 -0500

    Move Stuff Around, Finish MainMenu Layout

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    private void StartGame(string levelToLoad)
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    private void QuitGame()
+    {
+        Application.Quit();
+    }
+}
