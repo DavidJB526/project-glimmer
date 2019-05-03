9009acbb9a3de3c1a2c391742ee890e04531964d
DavidJB526 <david.beaudoin@loop.colum.edu>
2019-05-03 01:06:41 -0500

    Generate Lighting Data, "Finishing" Touches

--- Assets/Scripts/EndScene.cs
+++ Assets/Scripts/EndScene.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(BoxCollider))]
 public class EndScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Image blackScreenPanel;
+    [SerializeField]
+    private string sceneToLoad;
+    [SerializeField]
+    private float timeToFade;
+
+    private void Awake()
     {
-        
+        blackScreenPanel.CrossFadeAlpha(0, 0, true);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.tag == "Player")
+        {
+            blackScreenPanel.CrossFadeAlpha(1, timeToFade, true);
+            StartCoroutine(TriggerEnd());
+        }
+    }
+
+    IEnumerator TriggerEnd()
+    {
+        yield return new WaitForSeconds(timeToFade + .5f);
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
